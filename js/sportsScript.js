d25b2502e31431776fcca6a3ba6ce44523e8a443
iamQueenL <leshebaqueen@gmail.com>
2020-01-31 13:10:40 +0200

    updated sport section

--- js/sportsScript.js
+++ js/sportsScript.js
@@ -9,21 +9,27 @@ const soccer = document.querySelector(".soccer")
 const swim = document.querySelector(".swim")
 const basketball = document.querySelector(".basketball")
 const cricket = document.querySelector(".cricket")
+const form = document.querySelector('.sign');
+const notification = document.querySelector("#notification");
+
+// soccer.addEventListener('click',() =>{
+//     location.href="SportsArticle.html"
+// })
+
+// swim.addEventListener('click',() =>{
+//     location.href="SportsArticle.html"
+// })
+// basketball.addEventListener('click',() =>{
+//     location.href="SportsArticle.html"
+// })
+// cricket.addEventListener('click',() =>{
+//     location.href="SportsArticle.html"
+// })
 
 
-soccer.addEventListener('click',() =>{
-    location.href="SportsArticle.html"
+notification.addEventListener('click', e =>{
+    e.preventDefault();
+    form.style.display = "block";
 })
-
-swim.addEventListener('click',() =>{
-    location.href="SportsArticle.html"
-})
-basketball.addEventListener('click',() =>{
-    location.href="SportsArticle.html"
-})
-cricket.addEventListener('click',() =>{
-    location.href="SportsArticle.html"
-})
-
 
 
