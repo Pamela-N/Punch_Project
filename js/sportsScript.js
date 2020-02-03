920c714fe37ff4005c849450de6d645ae6dfd93a
iamQueenL <leshebaqueen@gmail.com>
2020-02-03 15:44:01 +0200

    Queens done project

--- js/sportsScript.js
+++ js/sportsScript.js
@@ -11,6 +11,7 @@ const basketball = document.querySelector(".basketball")
 const cricket = document.querySelector(".cricket")
 const form = document.querySelector('.sign');
 const notification = document.querySelector("#notification");
+const submit = document.querySelector(".signB")
 
 // soccer.addEventListener('click',() =>{
 //     location.href="SportsArticle.html"
@@ -31,5 +32,25 @@ notification.addEventListener('click', e =>{
     e.preventDefault();
     form.style.display = "block";
 })
+submit.addEventListener('click', e =>{
+    e.preventDefault();
+    form.style.display="none";
+})
+
+// PopUp code inspired by W3Schools and edited
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("mySidenav").style.display = "none";
+    document.getElementsByClassName("one").style.display = "none";
+
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("mySidenav").style.display = "block";
+    document.getElementsByClassName("one").style.display = "block";
+
+  }
 
 
