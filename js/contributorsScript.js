4fd2425eb6e0e1e34df67c5f33a09a59df4d7dca
iammosa <ramosamosa@gmail.com>
2020-02-07 13:11:10 +0200

    completed some seo things that dont need to be changed when live

--- js/contributorsScript.js
+++ js/contributorsScript.js
@@ -4,16 +4,15 @@ let notification = document.querySelector('#notification');
 let sbmit = document.querySelector(".signB");
 const form = document.querySelector('.sign');
 
+// how the side nav opens and close
 function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-
+  document.getElementById("mySidenav").style.left = "0vw";
 }
-
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-
+  document.getElementById("mySidenav").style.left = "-100vw";
 }
 
+// this is how the slide moves from side to side taken from w3schools
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -39,9 +38,6 @@ function showSlides(n) {
 
 }
 
-
-
-
 // PopUp code inspired by W3Schools and edited
 
 function openForm() {
@@ -58,13 +54,19 @@ function closeForm() {
 
 }
 
-// notification bell comands
+// notification bell comands for the popup
 notification.addEventListener('click', e =>{
   e.preventDefault();
   form.style.display = "block";
 })
 
-  sbmit.addEventListener('click', e =>{
-    e.preventDefault();
-    form.style.display = "none";
+sbmit.addEventListener('click', e =>{
+  e.preventDefault();
+  form.style.display = "none";
+  alert('Thank you for subscribing to Punch notifications, we will be sending you exciting updates on your cellphone number everyday from now on.')
 })
+
+// notification bell comands for the bell to dissappear
+setTimeout( () => {
+  notification.style.display = "none";
+}, 10000 )
